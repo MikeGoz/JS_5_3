e87a385c92d8baed90334135f111f8ec6da08b7f
MikeGoz <gozdz@onet.pl>
2017-10-04 20:04:47 +0200

    poprawka Task 5.3

--- server.js
+++ server.js
@@ -17,11 +17,11 @@ app.get('/getNote', function(req, res) {
   });
 });
 app.post('/update/:note', function (req, res) {
-    console.log('request :POST');
-    stringifyFile += req.params.note + "\r\n";   
-    fs.writeFile('./test.json', stringifyFile, function(err) {
-      if (err) throw err;
-      res.send('file test.json updated with : ' + req.params.note);
+  console.log('request :POST');
+  stringifyFile += req.params.note + "\r\n";   
+  fs.writeFile('./test.json', stringifyFile, function(err) {
+    if (err) throw err;
+    res.send('file test.json updated with : ' + req.params.note);
   });    
 });  
 app.use(function (req, res, next) {
